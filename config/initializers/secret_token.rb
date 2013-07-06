93add42ba5bd62f525d94935ab67c63eb9ba8a16
harrisontanh@gmail.com <harrisontanh@gmail.com>
2013-07-06 11:47:34 -0700

    Initial commit with rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theatr::Application.config.secret_token = 'ca1cc6aa50051dd7769f33d899c318a8e745c9d915f11fec7667bc93a768c87e2b2bb4f44db9715c41fbb9dbc8c9c38cc9d758d76bbc8e1c4b75e6793340faaa'
